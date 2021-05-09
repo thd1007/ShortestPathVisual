3dc24b5e65964fe8f53f2e1a0df6b802dfa90722
xfa4 <tranhaidang01dtm@gmail.com>
2021-05-09 14:42:05 +0700

    add coordinate, change neighbors visibility

--- JavaCode/ShortestPath/src/Elements/Node.java
+++ JavaCode/ShortestPath/src/Elements/Node.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Node implements Comparable<Node>{
+	// Coordinate
+	private int x;
+	private int y;
 	// Id for readability of result purposes
 	private static int idCounter = 0;
 	public int id;
@@ -13,38 +16,77 @@ public class Node implements Comparable<Node>{
 	
 	// Evaluation functions
 	public double f = Double.MAX_VALUE;
-    public double g = Double.MAX_VALUE;
+	public double g = Double.MAX_VALUE;
     // Heuristic
-    public double h;
+	public double h;
     
-    public Node(double h) {
-        this.h = h;
-        this.id = idCounter++;
-        this.neighbors = new ArrayList<>();
+	public Node(double h) {
+		this.h = h;
+		this.id = idCounter++;
+		this.neighbors = new ArrayList<>();
     }
     
     @Override
     public int compareTo(Node n) {
-          return Double.compare(this.f, n.f);
+    	return Double.compare(this.f, n.f);
     }
     
-	public List<Edge> neighbors;
+    private List<Edge> neighbors;
 	
-	public static class Edge {
-        Edge(int weight, Node node) {
-        	this.weight = weight;
-        	this.node = node;
-        }
+    public static class Edge {
+    	Edge(int weight, Node node) {
+    		this.weight = weight;
+    		this.node = node;
+    	}
+    	public int weight;
+    	public Node node;
+    }
+	
+    public List<Edge> getNeighbors() {
+		return neighbors;
+	}
 
-        public int weight;
-        public Node node;
+	public void setNeighbors(List<Edge> neighbors) {
+		this.neighbors = neighbors;
 	}
-	
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public static int getIdCounter() {
+		return idCounter;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Node getParent() {
+		return parent;
+	}
+
+	public double getF() {
+		return f;
+	}
+
+	public double getG() {
+		return g;
+	}
+
+	public double getH() {
+		return h;
+	}
+
 	public void addBranch(int weight, Node node)
-	{
-		Edge newEdge = new Edge(weight, node);
-		neighbors.add(newEdge);
-	}
+    {
+    	Edge newEdge = new Edge(weight, node);
+    	neighbors.add(newEdge);
+    }
 	
 	public double calculateHeuristic(Node target){
         return this.h;
