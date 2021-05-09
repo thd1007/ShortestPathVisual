01e8390641358ad3c48b4cf99bdd106dec72be17
xfa4 <tranhaidang01dtm@gmail.com>
2021-05-09 14:49:43 +0700

    Edit indent

--- JavaCode/ShortestPath/src/Elements/Node.java
+++ JavaCode/ShortestPath/src/Elements/Node.java
@@ -56,12 +56,12 @@ public class Node implements Comparable<Node>{
 
 
 	public void addBranch(int weight, Node node)
-    {
+	{
     	Edge newEdge = new Edge(weight, node);
     	neighbors.add(newEdge);
     }
 	
 	public double calculateHeuristic(Node target){
-        return this.h;
+		return this.h;
 	}
 }
